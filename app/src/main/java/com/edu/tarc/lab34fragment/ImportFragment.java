8c3e4d28fa9729e0368f807d7ca47551341e54fa
taruc <wonghl-wa16@student.tarc.edu.my>
2018-12-04 13:01:12 +0800

    done

--- app/src/main/java/com/edu/tarc/lab34fragment/ImportFragment.java
+++ app/src/main/java/com/edu/tarc/lab34fragment/ImportFragment.java
@@ -21,7 +21,7 @@ public class ImportFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
